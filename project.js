193ec2ddc4912eb5364c0c79c460641ec35886d0
fishboyraduk <rjayson@protonmail.com>
2023-09-26 15:34:19 +0300

    Give winnings and loop gamble until user prompts out

--- project.js
+++ project.js
@@ -141,11 +141,29 @@ const getWinnings = (rows, bet, lines) => {
     return winnings;
 };
 
-let balance = deposit();
-const numberOfLines = getNumberOfLines();
-const bet = getBet(balance, numberOfLines);
-const reels = spin();
-const rows = transpose(reels);
-printRows(rows);
-const winnings = getWinnings(rows, bet, numberOfLines);
-console.log("You Won, $" +winnings.toString());
+const game = () => {
+  let balance = deposit();
+
+  while (true) {
+    console.log("You have a balance of $"+balance);
+    const numberOfLines = getNumberOfLines();
+    const bet = getBet(balance, numberOfLines);
+    balance -= bet * numberOfLines;
+    const reels = spin();
+    const rows = transpose(reels);
+    printRows(rows);
+    const winnings = getWinnings(rows, bet, numberOfLines);
+    balance += winnings;
+    console.log("You Won, $" + winnings.toString());
+
+    if(balance <= 0) {
+        console.log("You have run out of money!");
+        break
+    }
+
+    const PlayAgain = prompt("Do you want to play again (y/n)?");
+
+    if (PlayAgain != "y") break;
+  }
+};
+game();
