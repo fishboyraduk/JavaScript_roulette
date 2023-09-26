8f3e33706eb531f8f76663eee23d654e8dcf7ae9
fishboyraduk <rjayson@protonmail.com>
2023-09-26 12:50:55 +0300

    created slot machine

--- project.js
+++ project.js
@@ -79,10 +79,22 @@ const spin = () => {
         for (let i = 0; i < count; i++){
             symbols.push(symbol);
         }
-    }  
+    }
+    const reels = [[],[],[]];
+    for (let i = 0; i < COLS; i++){
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++){
+            const randomIndex = Math.floor(Math.random()* reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+    return reels;
 };
   
-spin();
+const reels = spin();
+console.log(reels);
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
